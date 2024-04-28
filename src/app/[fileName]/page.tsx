a6c602a53351c2ec61f1b4be913663ba9686db0e
akshatbhansali <akshatb2198@gmail.com>
2024-04-28 19:59:23 +0530

    added iframe

--- src/app/[fileName]/page.tsx
+++ src/app/[fileName]/page.tsx
@@ -1,34 +1,42 @@
-
 import { Metadata } from "next";
-// import PDFViewer from "../components/PDFViewer" 
+// import PDFViewer from "../components/PDFViewer"
 // import PDFViewer from 'pdf-viewer-reactjs'
 // import { PDFReader } from 'reactjs-pdf-reader';
-type Props ={
-    params:{
-        fileName:String
-    }
-}
+type Props = {
+  params: {
+    fileName: String;
+  };
+};
 
-export const generateMetadata=({params}:Props):Metadata=>{
-    return {
-        title :`${params.fileName}`,
-        description:`pdf`
-    }
-}
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `${params.fileName}`,
+    description: `pdf`,
+  };
+};
 
-const page = ({params}:Props) => {
-    return (
-        <div className='page'>
-            {/* <PDFViewer url={"https://firebasestorage.googleapis.com/v0/b/zomato-clone-417913.appspot.com/o/"+params.fileName+"?alt=media"} /> */}
-            <iframe width={700} height={700} src={"https://ik.imagekit.io/caii01zbf/-28_4_2024-bill_sales-2.pdf?updatedAt=1714308797396"} title="description"></iframe>
-            {/* <PDFViewer
+const page = ({ params }: Props) => {
+  return (
+    <div className="page">
+      {/* <PDFViewer url={"https://firebasestorage.googleapis.com/v0/b/zomato-clone-417913.appspot.com/o/"+params.fileName+"?alt=media"} /> */}
+        <iframe
+          className=""
+          src={
+            "https://firebasestorage.googleapis.com/v0/b/zomato-clone-417913.appspot.com/o/" +
+            params.fileName +
+            "?alt=media"
+          }
+          title="description"
+        ></iframe>
+
+      {/* <PDFViewer
             document={{
                 url: `https://firebasestorage.googleapis.com/v0/b/zomato-clone-417913.appspot.com/o/${params.fileName}?alt=media`,
             }} */}
-        {/* /> */}
-        {/* <PDFReader url={"https://firebasestorage.googleapis.com/v0/b/zomato-clone-417913.appspot.com/o/"+params.fileName+"?alt=media"}/> */}
-        </div>
-    )
-}
+      {/* /> */}
+      {/* <PDFReader url={"https://firebasestorage.googleapis.com/v0/b/zomato-clone-417913.appspot.com/o/"+params.fileName+"?alt=media"}/> */}
+    </div>
+  );
+};
 
-export default page
+export default page;
