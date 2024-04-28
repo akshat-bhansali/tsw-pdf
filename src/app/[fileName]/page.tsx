0d869fae844c2bf4c1b91b2034538db2da75503a
akshatbhansali <akshatb2198@gmail.com>
2024-04-28 18:05:24 +0530

    changes

--- src/app/[fileName]/page.tsx
+++ src/app/[fileName]/page.tsx
@@ -16,7 +16,8 @@ export const generateMetadata=({params}:Props):Metadata=>{
 const page = ({params}:Props) => {
     return (
         <div className='page'>
-            <PDFViewer url={"https://firebasestorage.googleapis.com/v0/b/zomato-clone-417913.appspot.com/o/"+params.fileName+"?alt=media"} />
+            {/* <PDFViewer url={"https://firebasestorage.googleapis.com/v0/b/zomato-clone-417913.appspot.com/o/"+params.fileName+"?alt=media"} /> */}
+            <iframe width={700} height={700} src={"https://firebasestorage.googleapis.com/v0/b/zomato-clone-417913.appspot.com/o/"+params.fileName+"?alt=media"} title="description"></iframe>
         </div>
     )
 }
