1ba811d29b76c8cb8b04a1195f04dd2e30624af1
akshatbhansali <akshatb2198@gmail.com>
2024-04-28 07:13:48 +0530

    removed main page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
         imageUrl={"https://media.istockphoto.com/id/1370772148/photo/track-and-mountains-in-valle-del-lago-somiedo-nature-park-asturias-spain.jpg?s=612x612&w=0&k=20&c=QJn62amhOddkJSbihcjWKHXShMAfcKM0hPN65aCloco="}
         imageAlt={content.metaImageAlt}
       /> */}
-      hi
       </>
   );
 }
